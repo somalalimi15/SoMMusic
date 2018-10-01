62969704d05e12a001a710edbeafed2c683ff188
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-10-01 19:23:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,6 +24,26 @@ const prefix = "S";
 /////////////////////////
 ////////////////////////
 
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+if (command == "say") {
+if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return message.reply("*لا تملك الصلاحيات المطلوبه**");
+
+message.channel.send(args.join("  "))
+    message.delete();
+  }
+
+
+
+});
+
 client.on('message', async msg =>{
 	if (msg.author.bot) return undefined;
     if (!msg.content.startsWith(prefix)) return undefined;
